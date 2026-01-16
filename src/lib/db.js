5b51ec206b02f53cba05631e8043f63a559e4b78
hussain <mirzashahhussain63@gmail.com>
2026-01-16 14:28:30 +0530

    database driver chanage mysql2 to mysql

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,18 +1,46 @@
-import mysql from "mysql2/promise";
+// import mysql from "mysql2/promise";
 
-let connection;
-export const createConnection = async () => {
-  if (!connection) {
-    connection = await mysql.createConnection({
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
+// let connection;
+// export const createConnection = async () => {
+//   if (!connection) {
+//     connection = await mysql.createConnection({
+//       host: process.env.DATABASE_HOST,
+//       port: process.env.DATABASE_PORT,
+//       user: process.env.DATABASE_USER,
+//       password: process.env.DATABASE_PASSWORD,
+//       database: process.env.DATABASE_NAME,
+//       waitForConnections: true,
+//       connectionLimit: 10,
+//     });
+//   }
+
+//   return connection;
+// };
+
+
+import sql from "mssql";
+
+let pool;
+
+export async function createConnection() {
+  if (!pool) {
+    pool = await sql.connect({
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
+      server: process.env.DATABASE_HOST, 
       database: process.env.DATABASE_NAME,
-      waitForConnections: true,
-      connectionLimit: 10,
+      port: Number(process.env.DATABASE_PORT), 
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+      pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000,
+      },
     });
   }
 
-  return connection;
-};
+  return pool;
+}
